37b8da0d26308b968c6a6091ec8edb8b9b6d1993
Jeremy Sim <59082457+jsim0809@users.noreply.github.com>
2020-01-05 23:49:31 -0800

    Did two extra credit problems

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -63,7 +63,8 @@ describe("About Applying What We Have Learnt", function() {
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = _.range(1, 1000).reduce(function(memo, x) { if (x % 3 === 0 || x % 5 === 0 ) { return memo + x; } else { return memo + 0; } }, 0);    /* try chaining range() and reduce() */
+    var sum = _.range(1, 1000)
+    	.reduce(function(memo, x) { if (x % 3 === 0 || x % 5 === 0 ) { return memo + x; } else { return memo + 0; } }, 0);    /* try chaining range() and reduce() */
 
     expect(233168).toBe(sum);
   });
@@ -86,36 +87,89 @@ describe("About Applying What We Have Learnt", function() {
 
     /* chain() together map(), flatten() and reduce() */
 
-    var ingredientsCount = _(products).chain()
+    _(products).chain()
     	.map(function(food) { return food.ingredients })
     	.flatten()
-	    .reduce(function(memo, ingredient){ if(ingredient === "mushrooms") { return memo + 1; } else { return memo; }  }, 0);
+	    .reduce(function(memo, ingredient){ ingredientCount[ingredient] = (ingredientCount[ingredient] || 0) +1 }, "");
 
     expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
+  
   it("should find the largest prime factor of a composite number", function () {
   
+  	var findFactors = function(num) {
+  		var withOne= _.range(1, num)
+			  			.filter(function(x) { return num%x === 0 });
+			withOne.shift();
+		return withOne;
+  	}
+
+  	var isAHigherMultipleOf = function(num, factor) {
+  		return num/factor>1 && num%factor===0;
+  	}
+
+  	var isAHigherMultipleOfAnythingInAnArray = function(num, array) {
+  		var isIt = false;
+  		_(array).forEach(function(x){ if(isAHigherMultipleOf(num, x)) { isIt = true; } });
+  		return isIt;
+  	}
+
+
+  	var findLPF = function(num) {
+  		var allFactors = findFactors(num);
+  		var allFactors2 = findFactors(num);
+	  	var primeFactors = [];
+  		_(allFactors2).forEach(function(x) { if(!isAHigherMultipleOfAnythingInAnArray(x, allFactors)) { primeFactors.push(x); } });
+  		return primeFactors.pop();
+  	}
+
+	expect(findLPF(200)).toBe(5);
+
   });
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
     
+  	var isPalindrome = function(num) {
+  		var string = num.toString();
+  		var reversed = string.split('').reverse().join('');
+  		return string === reversed;
+  	}
+
+  	var palindromes = [];
+
+  	_.range(100,1000)
+  		.forEach(function(x) { _.range(100,1000)
+  									.forEach(function(y) { if (isPalindrome(x*y)) {
+  										palindromes.push(x*y);
+  									}
+  								})
+  								});
+
+  	var largestPalindrome = palindromes.pop();
+
+	expect(largestPalindrome).toBe(580085);
+
   });
 
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-      
+
+	expect(1).toBe(2);      
     
   });
 
   it("should find the difference between the sum of the squares and the square of the sums", function () {
     
+	expect(1).toBe(2);
+
   });
 
   it("should find the 10001st prime", function () {
 
+	expect(1).toBe(2);
+
   });
-  */
+  
 });
